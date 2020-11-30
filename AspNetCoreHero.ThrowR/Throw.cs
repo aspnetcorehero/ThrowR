12201be2ff14778f78662395465bc2265dde6c56
Mukesh Murugan <iammukeshm@gmail.com>
2020-11-30 22:35:42 +0530

    Add project files.

---
+++ AspNetCoreHero.ThrowR/Throw.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreHero.ThrowR
+{
+    public interface IThrow
+    {
+    }
+
+    public class Throw : IThrow
+    {
+        public static IThrow Exception { get; } = new Throw();
+
+        private Throw()
+        {
+        }
+    }
+}
